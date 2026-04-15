545b95e231718fd72342b50b3cc31ae5595eace7
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-15 06:52:30 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-15

--- app/LizeriumAccauntManager/Windows/AboutWindow.Designer.cs
+++ app/LizeriumAccauntManager/Windows/AboutWindow.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 апреля 2026 12:25:10
- * Version: 1.0.8
+ * Last Updated: 15 апреля 2026 06:52:25
+ * Version: 1.0.10
  */
 
 namespace Root

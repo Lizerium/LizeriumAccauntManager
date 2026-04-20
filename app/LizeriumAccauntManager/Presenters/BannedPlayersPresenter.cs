cff57aa6308a08211cb8f15351c2095c1008e521
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-20 03:07:09 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-19

--- app/LizeriumAccauntManager/Presenters/BannedPlayersPresenter.cs
+++ app/LizeriumAccauntManager/Presenters/BannedPlayersPresenter.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 апреля 2026 14:44:50
- * Version: 1.0.14
+ * Last Updated: 20 апреля 2026 03:07:02
+ * Version: 1.0.15
  */
 
 using System.Linq;

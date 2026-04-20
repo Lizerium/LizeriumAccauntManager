9cdfb746a57a70c010e523b44cb94bbdf4864cdb
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-20 16:22:21 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-20

--- app/LizeriumAccauntManager/Windows/Player Edit UI/AddCargoWindow.cs
+++ app/LizeriumAccauntManager/Windows/Player Edit UI/AddCargoWindow.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 апреля 2026 03:07:02
- * Version: 1.0.15
+ * Last Updated: 20 апреля 2026 16:22:14
+ * Version: 1.0.16
  */
 
 using System;

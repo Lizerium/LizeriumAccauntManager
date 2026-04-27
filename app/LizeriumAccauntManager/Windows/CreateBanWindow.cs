98aa0a646890d2f32fcb36e438b20cf802881e97
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-27 09:42:06 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-27

--- app/LizeriumAccauntManager/Windows/CreateBanWindow.cs
+++ app/LizeriumAccauntManager/Windows/CreateBanWindow.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 26 апреля 2026 09:56:57
- * Version: 1.0.22
+ * Last Updated: 27 апреля 2026 09:41:57
+ * Version: 1.0.23
  */
 
 using System;

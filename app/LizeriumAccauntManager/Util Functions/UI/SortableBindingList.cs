3d5e4a16fb9359555e3e8fdf68bdb9870bec3333
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-10 07:43:04 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-10

--- app/LizeriumAccauntManager/Util Functions/UI/SortableBindingList.cs
+++ app/LizeriumAccauntManager/Util Functions/UI/SortableBindingList.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 мая 2026 07:53:15
- * Version: 1.0.35
+ * Last Updated: 10 мая 2026 07:42:56
+ * Version: 1.0.36
  */
 
 using System.Collections.Generic;

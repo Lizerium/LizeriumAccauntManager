d03c8a67ffa8368b5601d2eb5f20a8dae5383972
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-11 10:39:11 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-11

--- app/LizeriumAccauntManager/Properties/AssemblyInfo.cs
+++ app/LizeriumAccauntManager/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 мая 2026 07:42:56
- * Version: 1.0.36
+ * Last Updated: 11 мая 2026 10:39:04
+ * Version: 1.0.37
  */
 
 using System.Reflection;

dde1fab7c04dcf7a9a3a201013c89a72f36f7fda
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-16 10:47:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-16

--- app/LizeriumAccauntManager/Windows/DecodeAllFilesWindow.Designer.cs
+++ app/LizeriumAccauntManager/Windows/DecodeAllFilesWindow.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 мая 2026 07:50:39
- * Version: 1.0.41
+ * Last Updated: 16 мая 2026 10:47:43
+ * Version: 1.0.42
  */
 
 namespace Root.Tool_UI

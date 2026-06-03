99e3dae1f9d063feafafe3c2f853ec211f66a98d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-03 15:17:23 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-03

--- app/LizeriumAccauntManager/Services/Logic/Char/ICharService.cs
+++ app/LizeriumAccauntManager/Services/Logic/Char/ICharService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 июня 2026 06:53:06
- * Version: 1.0.59
+ * Last Updated: 03 июня 2026 15:17:16
+ * Version: 1.0.60
  */
 
 using System.Threading.Tasks;

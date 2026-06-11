f222bccf1c03396e62b5009a4576ff9946cc3adf
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-11 13:27:36 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-11

--- app/LizeriumAccauntManager/Util Functions/FLUtility.cs
+++ app/LizeriumAccauntManager/Util Functions/FLUtility.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 июня 2026 06:53:04
- * Version: 1.0.67
+ * Last Updated: 11 июня 2026 13:27:28
+ * Version: 1.0.68
  */
 
 /*

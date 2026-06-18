b326b0d65a60005cdb5ddbffb9284ed618d28356
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-18 06:53:12 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-18

--- app/LizeriumAccauntManager/Services/FLHook/Socket/Event/IFLHookEventSocketService.cs
+++ app/LizeriumAccauntManager/Services/FLHook/Socket/Event/IFLHookEventSocketService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 июня 2026 06:52:56
- * Version: 1.0.74
+ * Last Updated: 18 июня 2026 06:53:05
+ * Version: 1.0.75
  */
 
 namespace Root.Services

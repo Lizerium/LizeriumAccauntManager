050265ed21ec80ce9f2ac5cb2c845744605a9cb9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-22 06:53:22 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-22

--- app/LizeriumAccauntManager/Views/BannedPlayers.Designer.cs
+++ app/LizeriumAccauntManager/Views/BannedPlayers.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 июня 2026 06:52:56
- * Version: 1.0.78
+ * Last Updated: 22 июня 2026 06:53:15
+ * Version: 1.0.79
  */
 
 using Root.Components;

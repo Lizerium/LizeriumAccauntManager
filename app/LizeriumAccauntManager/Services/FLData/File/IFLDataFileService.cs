b86ac2f091ff66c3b0dce217bbe071a2f796daab
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-12 06:53:13 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-12

--- app/LizeriumAccauntManager/Services/FLData/File/IFLDataFileService.cs
+++ app/LizeriumAccauntManager/Services/FLData/File/IFLDataFileService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 июля 2026 14:01:41
- * Version: 1.0.98
+ * Last Updated: 12 июля 2026 06:53:06
+ * Version: 1.0.99
  */
 
 using System.Collections.Generic;

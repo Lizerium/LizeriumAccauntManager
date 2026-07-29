a96089a093739529bc60af8f7676418ce9d71521
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-29 15:45:51 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-29

--- app/LizeriumAccauntManager.Tests/MSTestSettings.cs
+++ app/LizeriumAccauntManager.Tests/MSTestSettings.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 июля 2026 10:10:50
- * Version: 1.0.115
+ * Last Updated: 29 июля 2026 15:45:43
+ * Version: 1.0.116
  */
 
 [assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]

65d2fa571178ae980fea39eab945a5299d80aa8c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-19 10:23:09 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-19

--- app/LizeriumAccauntManager/Services/FLData/File/Implemenets/FLDataFileSetting.cs
+++ app/LizeriumAccauntManager/Services/FLData/File/Implemenets/FLDataFileSetting.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 августа 2026 06:53:14
- * Version: 1.0.136
+ * Last Updated: 19 августа 2026 10:23:01
+ * Version: 1.0.137
  */
 
 using System;

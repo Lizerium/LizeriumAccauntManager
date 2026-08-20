21cd3d33e28f5e64755e36dc295df50fef3a9c0c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-20 09:37:26 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-20

--- app/LizeriumAccauntManager/Components/UIData/CharUIInfo.cs
+++ app/LizeriumAccauntManager/Components/UIData/CharUIInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 19 августа 2026 10:23:01
- * Version: 1.0.137
+ * Last Updated: 20 августа 2026 09:37:18
+ * Version: 1.0.138
  */
 
 namespace Root.Components

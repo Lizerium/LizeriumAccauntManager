4dc372187799dc958e9a6d836b7c71faf1253411
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-25 06:53:14 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-25

--- app/LizeriumAccauntManager/Services/FLHook/Socket/Listener/Implements/FLHookListener.cs
+++ app/LizeriumAccauntManager/Services/FLHook/Socket/Listener/Implements/FLHookListener.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 августа 2026 06:53:10
- * Version: 1.0.142
+ * Last Updated: 25 августа 2026 06:53:07
+ * Version: 1.0.143
  */
 
 using System;

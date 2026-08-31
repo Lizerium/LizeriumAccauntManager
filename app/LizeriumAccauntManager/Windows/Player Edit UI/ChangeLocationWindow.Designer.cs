5ca0edf472334d23119d5363beb6510d2e681281
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-31 06:52:48 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-31

--- app/LizeriumAccauntManager/Windows/Player Edit UI/ChangeLocationWindow.Designer.cs
+++ app/LizeriumAccauntManager/Windows/Player Edit UI/ChangeLocationWindow.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 августа 2026 06:53:13
- * Version: 1.0.148
+ * Last Updated: 31 августа 2026 06:52:42
+ * Version: 1.0.149
  */
 
 using Root.Components;

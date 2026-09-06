d0f77e8d3f8efa36309699b4705275d399f849d5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-06 11:08:53 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-06

--- app/LizeriumAccauntManager.Tests/DataBaseServiceTests.cs
+++ app/LizeriumAccauntManager.Tests/DataBaseServiceTests.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 сентября 2026 08:36:55
- * Version: 1.0.154
+ * Last Updated: 06 сентября 2026 11:08:45
+ * Version: 1.0.155
  */
 
 using Dapper;
